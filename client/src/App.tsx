8430669911318064da021daf6ed64c946c7e6d49
basscee1604 <basscee1604@gmail.com>
2026-03-09 11:08:35 +0000

    fix: wrap app in Web3Provider to prevent runtime crash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Web3Provider } from "@/lib/Web3Context";
 import NotFound from "@/pages/not-found";
 
 import { LandingPage } from "@/pages/LandingPage";
@@ -31,10 +32,12 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
+      <Web3Provider>
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </Web3Provider>
     </QueryClientProvider>
   );
 }
